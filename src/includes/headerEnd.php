b007b184e3bc3df1262053e7f7657cc799ad72f3
DT1 <kf4hfc7hpp@privaterelay.appleid.com>
2026-03-24 17:08:00 +0100

    Update headerEnd.php

--- src/includes/headerEnd.php
+++ src/includes/headerEnd.php
@@ -3,24 +3,26 @@
     <body>
         <header>
             <nav class="navbar">
-                <div class="logo">
-                    <img class="img_logo" src="./images/logo2.png" alt="logo">
-                    Ethy<span>Connect</span>
+                <div class="navbar_div">
+                    <div class="logo">
+                        <img class="img_logo" src="./images/logo2.png" alt="logo">
+                        Ethy<span>Connect</span>
+                    </div>
+                    <ul class="nav-links">
+                        <li>
+                            <a href="index.php">Accueil</a>
+                        </li>
+                        <li>
+                            <a href="apropos.php">A propos</a>
+                        </li>
+                        <li>
+                            <a href="conduire.php">Conduire ?</a>
+                        </li>
+                        <li>
+                            <a href="classement.php">Classement</a>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="nav-links">
-                    <li>
-                        <a href="index.php">Accueil</a>
-                    </li>
-                    <li>
-                        <a href="apropos.php">A propos</a>
-                    </li>
-                    <li>
-                        <a href="conduire.php">Conduire ?</a>
-                    </li>
-                    <li>
-                        <a href="classement.php">Classement</a>
-                    </li>
-                </ul>
             </nav>
         </header>
         <main>
