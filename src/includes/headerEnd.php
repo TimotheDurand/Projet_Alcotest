0efa12be4e13dde35ad211380816c61b47adbe8a
DT1 <kf4hfc7hpp@privaterelay.appleid.com>
2026-03-27 10:43:09 +0100

    Update headerEnd.php

--- src/includes/headerEnd.php
+++ src/includes/headerEnd.php
@@ -1,28 +1,32 @@
     <link rel="stylesheet" href="css/style_navbar.css">
+    <script src="js/script_navbar.js" defer></script>
 </head>
     <body>
         <header>
-            <nav class="navbar">
-                <div class="navbar_div">
-                    <div class="logo">
-                        <img class="img_logo" src="./images/logo2.png" alt="logo">
-                        Ethy<span>Connect</span>
+                <nav class="navbar">
+                    <div class="navbar_div">
+                        <div class="logo">
+                            <img class="img_logo" src="./images/logo2.png" alt="logo">
+                            Ethy<span>Connect</span>
+                        </div>
+                        <div class="burger" id="burger">
+                            ☰
+                        </div>
+                        <ul class="nav-links" id="navLinks">
+                            <li>
+                                <a href="index.php">Accueil</a>
+                            </li>
+                            <li>
+                                <a href="apropos.php">A propos</a>
+                            </li>
+                            <li>
+                                <a href="conduire.php">Conduire ?</a>
+                            </li>
+                            <li>
+                                <a href="classement.php">Classement</a>
+                            </li>
+                        </ul>
                     </div>
-                    <ul class="nav-links">
-                        <li>
-                            <a href="index.php">Accueil</a>
-                        </li>
-                        <li>
-                            <a href="apropos.php">A propos</a>
-                        </li>
-                        <li>
-                            <a href="conduire.php">Conduire ?</a>
-                        </li>
-                        <li>
-                            <a href="classement.php">Classement</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+                </nav>
+            </header>
         <main>
